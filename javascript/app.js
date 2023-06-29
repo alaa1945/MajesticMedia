e3b0a42b6f2c1a947eabce55b9a0b525a97486b2
Alaa A.E <80849327+alaa1945@users.noreply.github.com>
2023-06-29 12:35:20 +0300

    Second

--- javascript/app.js
+++ javascript/app.js
@@ -1,49 +1,70 @@
-window.addEventListener('scroll', function() {
-         var navbar = document.querySelector('.navbar');
-         var navlink=document.querySelectorAll('.nav-link');
-         var activeEditorNavbar = document.querySelector('.elementor-editor-active .navbar');
-       
-         if (document.documentElement.scrollTop > 85 || document.body.scrollTop > 85) {
-           navbar.classList.add('navcolor');
-           navlink.forEach(function(element) {
-                  element.classList.add('col');
-                });
+window.addEventListener("scroll", function () {
+  var navbar = document.querySelector(".navbar");
+  var navlink = document.querySelectorAll(".nav-link");
+  var navbarNav=document.querySelector("#navbarNav");
+  var activeEditorNavbar = document.querySelector(
+    ".elementor-editor-active .navbar"
+  );
+  var navbar_toggler = document.querySelector(".nav-icon");
+  if (document.documentElement.scrollTop > 85 || document.body.scrollTop > 85) {
+    navbar.classList.add("navcolor");
+    navbar_toggler.classList.add("nav-scroll");
+    navbarNav.classList.add('navcolor');
+    navlink.forEach(function (element) {
+      element.classList.add("col");
+    });
 
-           if (activeEditorNavbar) {
-             activeEditorNavbar.classList.remove('navcolor');
-             activeEditorNavbar.classList.remove('fixed-top');
-           }
-         } else {
-           navbar.classList.remove('navcolor');
-           navlink.forEach(function(element) {
-                  element.classList.remove('col');
-                });
-         }
-       });
+    if (activeEditorNavbar) {
+      activeEditorNavbar.classList.remove("navcolor");
+      activeEditorNavbar.classList.remove("fixed-top");
+    }
+  } else {
+    navbar_toggler.classList.remove('nav-scroll');
+   navbar.classList.remove("navcolor");
+    navlink.forEach(function (element) {
+      element.classList.remove("col");
+    });
+  }
+});
 
-       var slideIndex = 1;
-       showDivs(slideIndex);
-   
-       function plusDivs(n) {
-         showDivs(slideIndex += n);
-       }
-   
-       function currentDiv(n) {
-         showDivs(slideIndex = n);
-       }
-   
-       function showDivs(n) {
-         var i;
-         var x = document.getElementsByClassName("mySlides");
-         var dots = document.getElementsByClassName("demo");
-         if (n > x.length) {slideIndex = 1}
-         if (n < 1) {slideIndex = x.length}
-         for (i = 0; i < x.length; i++) {
-           x[i].style.display = "none";  
-         }
-         for (i = 0; i < dots.length; i++) {
-           dots[i].className = dots[i].className.replace(" w3-white", "");
-         }
-         x[slideIndex-1].style.display = "block";  
-         dots[slideIndex-1].className += " w3-white";
-       }
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function currentDiv(n) {
+  showDivs((slideIndex = n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " w3-white";
+}
+
+const serviceItems = document.querySelectorAll('.service');
+    serviceItems.forEach((item) => {
+      const viewLink = item.querySelector('#view-link');
+      const expandedText = item.querySelector('.expanded-p');
+  
+      viewLink.addEventListener('click', () => {
+        expandedText.classList.toggle('expanded-p');
+        viewLink.querySelector('img').classList.toggle('rotate');
+      });
+    });
